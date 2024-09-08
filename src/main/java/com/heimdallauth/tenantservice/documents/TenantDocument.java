f51d0b285bbfe41574871b5e43a832225b826330
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-08 17:16:46 +0530

    improve(TenantCreationRequest): HTS-1 HTS-9 Create Tenant Information Mongo Document

---
+++ src/main/java/com/heimdallauth/tenantservice/documents/TenantDocument.java
@@ -0,0 +1,29 @@
+package com.heimdallauth.tenantservice.documents;
+
+import com.heimdallauth.tenantservice.constants.TenantStatus;
+import com.heimdallauth.tenantservice.models.TenantContactInformation;
+import com.heimdallauth.tenantservice.utils.ResourceIdentifier;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Document
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TenantDocument {
+    @Id
+    private ResourceIdentifier id;
+    private String tenantName;
+    private String tenantDescription;
+    private TenantContactInformation contactInformation;
+    private TenantStatus tenantStatus;
+    private Instant createdOn;
+    private Instant updatedOn;
+}
