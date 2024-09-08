fea24be4219cf8f7af648a298817ab23b10606e7
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-08 21:50:35 +0530

    improve(RestAPIDocs): HTS-1 HTS-2 Update to document the RestAPI

--- src/main/java/com/heimdallauth/tenantservice/controller/AccountsController.java
+++ src/main/java/com/heimdallauth/tenantservice/controller/AccountsController.java
@@ -2,6 +2,9 @@ package com.heimdallauth.tenantservice.controller;
 
 import com.heimdallauth.tenantservice.dto.AccountCreationRequestDTO;
 import com.heimdallauth.tenantservice.services.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +22,12 @@ public class AccountsController {
         this.accountService = accountService;
     }
 
+    @Operation(summary = "Create a new account", description = "Creates a new account with the provided details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Account created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/create")
     public ResponseEntity<String> createNewAccount(@RequestBody AccountCreationRequestDTO accountCreationRequestDTO){
         return ResponseEntity.ok(this.accountService.createNewAccount(accountCreationRequestDTO));
