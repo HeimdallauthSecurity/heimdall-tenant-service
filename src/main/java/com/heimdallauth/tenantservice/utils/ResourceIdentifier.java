a4e123059dd92c91b6b24dbfcb993d291060e581
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-08 22:35:41 +0530

    improve(RestAPIDocs): HTS-1 HTS-2 Update to the ResourceIdentifier format

--- src/main/java/com/heimdallauth/tenantservice/utils/ResourceIdentifier.java
+++ src/main/java/com/heimdallauth/tenantservice/utils/ResourceIdentifier.java
@@ -1,21 +1,24 @@
 package com.heimdallauth.tenantservice.utils;
 
+import com.heimdallauth.tenantservice.constants.ResourceType;
+
+import java.net.ResponseCache;
 import java.util.UUID;
 
 public class ResourceIdentifier {
     private static final String IDENTIFIER_PREFIX = "hrn";
+    private static final String PARTITION = "heimdallauth";
+    private static final String SERVICE = "tenant-service";
     private static final String DELIMITER = "::";
     private static final String RESOURCE_DELIMITER = "/";
 
-    private final String partition;
     private final String service;
     private final String region;
     private final String accountId;
     private final String resourceType;
     private final String resourceId;
 
-    public ResourceIdentifier(String partition, String service, String region, String accountId, String resourceType, String resourceId) {
-        this.partition = partition;
+    public ResourceIdentifier(String service, String region, String accountId, String resourceType, String resourceId) {
         this.service = service;
         this.region = region;
         this.accountId = accountId;
@@ -23,32 +26,33 @@ public class ResourceIdentifier {
         this.resourceId = resourceId;
     }
 
-    public static ResourceIdentifier buildResourceIdentifier(String accountId, String deploymentRegion, UUID tenantUUID) {
-        return ResourceIdentifier.fromString(String.format("hrn::heimdall::tenants::%s::%s/%s",deploymentRegion, accountId, tenantUUID.toString()));
+    public static ResourceIdentifier buildTenantIdResourceIdentifier(String accountId, String region) {
+        return new ResourceIdentifier("tenant-service", region, accountId, ResourceType.TENANT.toString(), UUID.randomUUID().toString());
     }
-    public static ResourceIdentifier fromString(String identifier) {
-        if (!identifier.startsWith(IDENTIFIER_PREFIX + DELIMITER)) {
-            throw new IllegalArgumentException("Invalid identifier format");
-        }
 
-        String[] parts = identifier.split(DELIMITER);
-        if (parts.length != 5) {
-            throw new IllegalArgumentException("Invalid identifier format");
-        }
-
-        String[] resourceParts = parts[4].split(RESOURCE_DELIMITER);
-        if (resourceParts.length != 2) {
-            throw new IllegalArgumentException("Invalid resource format");
-        }
-
-        return new ResourceIdentifier(parts[0], parts[1], parts[2],parts[3], resourceParts[0], resourceParts[1]);
-    }
+//    public static ResourceIdentifier fromString(String identifier) {
+//        if (!identifier.startsWith(IDENTIFIER_PREFIX + DELIMITER)) {
+//            throw new IllegalArgumentException("Invalid identifier format");
+//        }
+//
+//        String[] parts = identifier.split(DELIMITER);
+//        if (parts.length != 5) {
+//            throw new IllegalArgumentException("Invalid identifier format");
+//        }
+//
+//        String[] resourceParts = parts[4].split(RESOURCE_DELIMITER);
+//        if (resourceParts.length != 2) {
+//            throw new IllegalArgumentException("Invalid resource format");
+//        }
+//
+//        return new ResourceIdentifier(parts[3], parts[4], parts[3],parts[3], resourceParts[0], resourceParts[1]);
+//    }
 
     @Override
     public String toString() {
         return IDENTIFIER_PREFIX + DELIMITER +
-                partition + DELIMITER +
-                service + DELIMITER +
+                PARTITION + DELIMITER +
+                SERVICE + DELIMITER +
                 region + DELIMITER +
                 accountId + RESOURCE_DELIMITER +
                 resourceType + RESOURCE_DELIMITER +
