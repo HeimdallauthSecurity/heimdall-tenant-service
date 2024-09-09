c61092e5280a3d3a00c2dc064adcde6fe0959810
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-09 20:30:41 +0530

    improve(Accounts): HTS-1 HTS-2 Add Exception to throw when account is not found

---
+++ src/main/java/com/heimdallauth/tenantservice/exceptions/AccountNotFoundException.java
@@ -0,0 +1,9 @@
+package com.heimdallauth.tenantservice.exceptions;
+
+public class AccountNotFoundException extends RuntimeException {
+    public final String accountId;
+    public AccountNotFoundException(String message, String accountId) {
+        super(message);
+        this.accountId = accountId;
+    }
+}
