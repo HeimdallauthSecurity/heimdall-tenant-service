3499b395e4c632f2fa9440e2a5b7e69909367cd6
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-09-19 06:54:54 -0600

    task(AccountStatus): HTS-10 HTS-11 Update the creation payload to have account status

--- src/main/java/com/heimdallauth/tenantservice/dto/kratos/KratosUserAccountCreationDTO.java
+++ src/main/java/com/heimdallauth/tenantservice/dto/kratos/KratosUserAccountCreationDTO.java
@@ -4,6 +4,7 @@ public record KratosUserAccountCreationDTO(
         String username,
         String email,
         String firstName,
-        String lastName
+        String lastName,
+        boolean isActive
 ) {
 }
